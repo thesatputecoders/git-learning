91a9e9aad46ae3e74c2c354514012548402ae71a
Jayesh Satpute <jayeshsatputehpl@gmail.com>
2025-04-05 14:26:42 +0530

    updated the values from vipul to shri

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String []args){
         System.out.println("Jayesh");
-        System.out.println("vipul");
+        System.out.println("shri");
         System.out.println("pavan");
     }
 }
