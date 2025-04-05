5def043f68c0683608b2726cb8005e56e2d0a752
Jayesh Satpute <jayeshsatputehpl@gmail.com>
2025-04-05 15:44:02 +0530

    updated files

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String []args){
-        System.out.println("Jayesh");
-        System.out.println("shri");
-        System.out.println("pavan");
+        System.out.println("Day is runny");
     }
 }
