1689c12091a49f3a1a81743bc9119de6bd013e94
Jayesh Satpute <jayeshsatputehpl@gmail.com>
2025-04-05 14:12:50 +0530

    updated the Main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String []args){
         System.out.println("Jayesh");
+        System.out.println("vipul");
+        System.out.println("pavan");
     }
 }
