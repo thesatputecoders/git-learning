c4934330824cc4b4e394468231aa7644a031da52
Jayesh Satpute <112201379+thesatputecoders@users.noreply.github.com>
2025-04-08 00:35:24 +0530

    Update Main.java
    added lines to understand git fetch and pull

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String []args){
         System.out.println("Day is runny");
+        System.out.println("Day is sunday");
+        System.out.println("Day is monday");
     }
 }
